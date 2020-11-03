6ca43d21e63cfc54f1c199b5460a905747e4424c
hrfm0928 <tokyo_sti@yahoo.co.jp>
2020-11-03 09:42:52 +0900

    PHPファイル

---
+++ blog.php
@@ -0,0 +1,19 @@
+<?php
+
+$blog = $_POST;
+
+if ($blog ['publish_status'] === 'publish'){
+    foreach($blog as $key => $value) {
+        echo '<pre>';
+        echo $key . ':' . htmlspecialchars($value,ENT_QUOTES,'UTF-8');
+        echo '<pre>';
+    }
+}elseif  ($blog ['publish_status'] === 'un_publish'){
+    echo "公開中の記事はありません。";
+} else {
+    echo "記事はありません。";
+} 
+
+
+
+?>
